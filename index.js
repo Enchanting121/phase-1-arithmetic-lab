33ab077a38c0cb1f10053b075cc0e93dc2a8d570
Enchanting121 <samantha.barros95@gmail.com>
2022-02-26 15:17:35 -0500

    commit tree d9fdeab033128dd770ab6a34e8d77e7ff0b3ca96

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = 0
 
-// Write your code here
+multiply = num1 * num2;
+
+let random = 0;
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 0;
+let num3 = 4;
+let num4 = 16;
+
+mod = num3 % num4;
+
+let max = 0;
+max = Math.max(1, 2, 3, 4, 20);
